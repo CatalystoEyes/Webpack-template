f82d4a1a8765d27b71c978a42fb4ebbe5e6ecf06
CatalystoEyes <xmajioff@gmail.com>
2024-08-03 19:32:24 +0300

    Fix html-class styling

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,10 +1,17 @@
-import "./app.module.scss";
+import styles from "./App.module.scss"
+import reactIcon from '../../assets/react.png'
+import typescriptIcon from '../../assets/typescript.png'
+import webpackIcon from '../../assets/webpack.png'
 
 export const App = () => {
   return (
-    <div className="container">
-      <h1 className="main-text">Webpack template</h1>
-      <p className="small-text">React</p>
+    <div className={styles.container}>
+      <h1 className={styles.main_text}>Webpack template</h1>
+      <div className={styles.images}>
+        <img src={reactIcon} alt="react" />
+        <img src={typescriptIcon} alt="typescript" />
+        <img src={webpackIcon} alt="webpack" />
+      </div>
     </div>
   );
 };
